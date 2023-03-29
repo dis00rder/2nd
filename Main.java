784cb6930d4cdea5a97412ba1238d5f9618541ab
dmytro.lehenkiy <disorder.ne@gmail.com>
2023-03-29 12:41:05 +0200

    5 dz

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello1");
+
+        Second Second2 = new Second();
+
+        Second.printMethod();
+
     }
+
 }
---
+++ Second.java
@@ -0,0 +1,11 @@
+public class Second {
+    static int a = 10;
+         public static int printMethod() {
+            System.out.println(a);
+            return a;
+
+        }
+
+}
+
+
